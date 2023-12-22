f839e4c7a4b0fffa88459d6267688c3ec0daf24d
Sabbyasachi Chakraborty <chakrabortysabbyasachi@gmail.com>
2023-12-22 13:17:15 +0530

    updated grid of cards

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Navbar searchdata={""}/>
       <Hero/>
-     <Section/>
+     <Section type='new'/>
     </div>
   );
 }
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,15 +3,15 @@ import { Button, Stack, Typography, Grid } from '@mui/material'
 import Styles from './Section.module.css'
 import Card from '../Card/Card';
 import axios from 'axios';
-const Section = () => {
-    const [ topAlbums, setTopAlbums ] = useState([]);
+const Section = ({type}) => {
+    const [ albums, setAlbums ] = useState([]);
     
     useEffect(()=>{
-        const fetchData= async()=>{
+        const fetchData= async()=>{ 
             try{
-                const {data} = await axios.get('https://qtify-backend-labs.crio.do/albums/top');
+                const {data} = await axios.get(`https://qtify-backend-labs.crio.do/albums/${type=='top'?'top':'new'}`);
                 console.log(data);
-                setTopAlbums(data);
+                setAlbums(data);
             }catch(err){
                 console.log(err)
             }
@@ -27,22 +27,23 @@ const Section = () => {
                 alignItems="center"
                 mb='10px'
             >
-                <Typography sx={{ fontSize: '20px', fontWeight: '600' }}>Top Albums</Typography>
+                <Typography sx={{ fontSize: '20px', fontWeight: '600' }}>{type=='top'?'Top':'New'} Albums</Typography>
                 <Button sx={{ color: '#34c94b', fontSize: '20px', fontWeight: '600', textTransform: 'none' }}>Collapse</Button>
             </Stack>
-            <Grid container spacing={4} justifyContent="space-between">
-
-                {
-                    topAlbums && topAlbums.map(album => {
-                        console.log(album.id)
-                        return (
-                            <Grid item >
-                                <Card data={album} type='album'/>
-                            </Grid>
-                        )
-                    })
-                }
-            </Grid>
+            <div className={Styles.grid_wrapper}>
+                <Grid container spacing={8} justifyContent="start">
+                    {
+                        albums && albums.map(album => {
+                            console.log(album.id)
+                            return (
+                                <Grid item >
+                                    <Card data={album} type='album'/>
+                                </Grid>
+                            )
+                        })
+                    }
+                </Grid>
+            </div>
         </div>
     )
 }
